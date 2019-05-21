a967d1a5f4bc09a6ae8175cedb69ea27c8f5639e
Thwarikh Anvar T S <48545060+thwarikh@users.noreply.github.com>
2019-05-22 00:48:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,6 +27,8 @@ if __name__ == "__main__" :
         session_name=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
-        plugins_dir="plugins"
+        plugins = dict(
+        root="plugins"
+    )
     )
     app.run()
